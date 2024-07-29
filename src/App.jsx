73b69228496a24ce7cee410b126e07e683f63335
robertomiranda <mirandaroberto1994@gmail.com>
2024-07-29 20:48:40 -0300

    agrega rutas protegidas

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { AuthProvider } from './context/AuthProvider';
-import Login from './components/Login';
-import Profile from './components/Profile';
-import Home from './components/Home';
-import ErrorPage from './components/ErrorPage';
+// App.jsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthProvider";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Home from "./components/Home";
+import ErrorPage from "./components/ErrorPage";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 const App = () => {
-  return (
-    <Router>
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </AuthProvider>
-    </Router>
-  );
+    return (
+        <Router>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route
+                        path="/profile"
+                        element={
+                            <ProtectedRoute>
+                                <Profile />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route path="*" element={<ErrorPage />} />
+                </Routes>
+            </AuthProvider>
+        </Router>
+    );
 };
 
 export default App;
---
+++ src/routes/Router.jsx
@@ -0,0 +1,45 @@
+import { createBrowserRouter } from "react-router-dom";
+import ArticleForm from "../components/ArticleForm";
+import Home from "../components/Home";
+import Login from "../components/Auth/Login";
+import Layout from "./Layout";
+import ProtectedRoute from "./ProtectedRoute";
+
+const Router = createBrowserRouter([
+    {
+        element: <Layout />,
+        children: [
+            {
+                index: true, // path: "/"
+                element: <Home />,
+            },
+            {
+                path: "articles",
+                children: [
+                    {
+                        index: true,
+                        element: <h1>Artículos</h1>,
+                    },
+                    {
+                        path: "add",
+                        element: (
+                            <ProtectedRoute>
+                                <ArticleForm />
+                            </ProtectedRoute>
+                        ),
+                    },
+                ],
+            },
+            {
+                path: "login",
+                element: <Login />,
+            },
+        ],
+    },
+    {
+        path: "*",
+        element: <h1>Not Found</h1>,
+    },
+]);
+
+export { Router };
